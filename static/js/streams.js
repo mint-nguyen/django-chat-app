d570e482df3fc19a97e337c266810daf29217e0b
Mint <mintnguyen.pa@gmail.com>
2023-01-02 11:48:22 -0800

    feat: create member, get member

--- static/js/streams.js
+++ static/js/streams.js
@@ -1,4 +1,4 @@
-const APP_ID = "YOUR APP ID";
+const APP_ID = "app id";
 const TOKEN = sessionStorage.getItem("token");
 const CHANNEL = sessionStorage.getItem("room");
 let UID = sessionStorage.getItem("UID");
